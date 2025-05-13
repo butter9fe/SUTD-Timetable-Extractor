9c66ee082c8ececa407c20c17d988fcb1396b9b0
Butterknife <82465661+butter9fe@users.noreply.github.com>
2025-05-13 18:38:00 +0800

    Tweak: Added more room names (Thanks nicholas!)

--- CalendarExtractor.js
+++ CalendarExtractor.js
@@ -13,24 +13,54 @@ const nameOfFile = "schedule";
 	const keepCourseType = true; // Whether you want to keep course type in the name (eg: Lecture/Cohort Based Learning)
 
 	const roomNames = {
-		"1.314": "Cohort Class 1",
-		"1.315": "Cohort Class 2",
-		"1.413": "Cohort Class 3",
-		"1.414": "Cohort Class 4",
-		"1.513": "Cohort Class 5",
-		"1.514": "Cohort Class 6",
-		"1.608": "Cohort Class 7",
-		"1.609": "Cohort Class 8",
-		"2.307": "Cohort Class 9",
-		"2.308": "Cohort Class 10",
-		"2.405": "Cohort Class 11",
-		"2.406": "Cohort Class 12",
-		"2.506": "Cohort Class 13",
-		"2.507": "Cohort Class 14",
-		"2.606": "Cohort Class 15",
-		"2.607": "Cohort Class 16",
-		"2.101": "Auditorium",
-		"1.102": "Albert Hong Lecture Theatre"
+	    "1.308": "Think Tank 1",
+	    "1.309": "Think Tank 2",
+	    "1.310": "Think Tank 3",
+	    "1.312": "Think Tank 4",
+	    "1.313": "Think Tank 5",
+	    "1.408": "Think Tank 6",
+	    "1.409": "Think Tank 7",
+	    "1.410": "Think Tank 8",
+	    "1.415": "Think Tank 9",
+	    "1.416": "Think Tank 10",
+	    "1.503": "Think Tank 11",
+	    "1.506": "Think Tank 12",
+	    "1.508": "Think Tank 13",
+	    "1.509": "Think Tank 14",
+	    "1.510": "Think Tank 15",
+	    "2.201": "Think Tank 16",
+	    "2.202": "Think Tank 17",
+	    "2.203": "Think Tank 18",
+	    "2.304": "Think Tank 19",
+	    "2.305": "Think Tank 20",
+	    "2.310": "Think Tank 21",
+	    "2.311": "Think Tank 22",
+	    "2.413": "Think Tank 23",
+	    "2.503": "Think Tank 24",
+	    "2.504": "Think Tank 25",
+	    "2.514": "Think Tank 26",
+	    "1.313": "Cohort Class 1",
+	    "1.314": "Cohort Class 2",
+	    "1.413": "Cohort Class 3",
+	    "1.414": "Cohort Class 4",
+	    "1.513": "Cohort Class 5",
+	    "1.514": "Cohort Class 6",
+	    "1.608": "Cohort Class 7",
+	    "1.609": "Cohort Class 8",
+	    "2.307": "Cohort Class 9",
+	    "2.308": "Cohort Class 10",
+	    "2.405": "Cohort Class 11",
+	    "2.406": "Cohort Class 12",
+	    "2.506": "Cohort Class 13",
+	    "2.507": "Cohort Class 14",
+	    "2.606": "Cohort Class 15",
+	    "2.607": "Cohort Class 16",
+	    "2.101": "Auditorium",
+	    "1.102": "Albert Hong Lecture Theatre 1",
+	    "1.203": "Lecture Theatre 2",
+	    "2.403": "Lecture Theatre 3",
+	    "2.404": "Lecture Theatre 4",
+	    "2.505": "Lecture Theatre 5"
 	}; // So that we can replace things like "ECC Building 1", feel free to add more!
 
 	const courseMisspellings = {
@@ -137,31 +167,31 @@ const nameOfFile = "schedule";
     const createdTime = `${currYear}${pad(now.getUTCMonth() + 1, 2)}${pad(now.getUTCDate(), 2)}T${pad(now.getUTCHours(), 2)}${pad(now.getUTCMinutes(), 2)}${pad(now.getUTCSeconds(), 2)}Z`;
 
     const classToEvent = c => `BEGIN:VEVENT
-DTSTAMP:${createdTime}
-UID:${Math.random().toString(16).slice(2)}@calendar.sutd.edu.sg
-DTSTART;TZID=Asia/Singapore:${currYear}${pad(c.day.month, 2)}${pad(c.day.day, 2)}T${pad(c.time[0], 4)}00
-DTEND;TZID=Asia/Singapore:${currYear}${pad(c.day.month, 2)}${pad(c.day.day, 2)}T${pad(c.time[1], 4)}00
-SUMMARY:${c.name}
-LOCATION:${c.place}
-END:VEVENT`;
+	DTSTAMP:${createdTime}
+	UID:${Math.random().toString(16).slice(2)}@calendar.sutd.edu.sg
+	DTSTART;TZID=Asia/Singapore:${currYear}${pad(c.day.month, 2)}${pad(c.day.day, 2)}T${pad(c.time[0], 4)}00
+	DTEND;TZID=Asia/Singapore:${currYear}${pad(c.day.month, 2)}${pad(c.day.day, 2)}T${pad(c.time[1], 4)}00
+	SUMMARY:${c.name}
+	LOCATION:${c.place}
+	END:VEVENT`;
 
     const icsContent = `BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:Academic Calendar ${currYear}
-CALSCALE:GREGORIAN
-BEGIN:VTIMEZONE
-TZID:Asia/Singapore
-TZURL:https://www.tzurl.org/zoneinfo-outlook/Asia/Singapore
-X-LIC-LOCATION:Asia/Singapore
-BEGIN:STANDARD
-TZNAME:+08
-TZOFFSETFROM:+0800
-TZOFFSETTO:+0800
-DTSTART:19700101T000000
-END:STANDARD
-END:VTIMEZONE
-${classes.map(classToEvent).join('\r\n')}
-END:VCALENDAR`;
+	VERSION:2.0
+	PRODID:Academic Calendar ${currYear}
+	CALSCALE:GREGORIAN
+	BEGIN:VTIMEZONE
+	TZID:Asia/Singapore
+	TZURL:https://www.tzurl.org/zoneinfo-outlook/Asia/Singapore
+	X-LIC-LOCATION:Asia/Singapore
+	BEGIN:STANDARD
+	TZNAME:+08
+	TZOFFSETFROM:+0800
+	TZOFFSETTO:+0800
+	DTSTART:19700101T000000
+	END:STANDARD
+	END:VTIMEZONE
+	${classes.map(classToEvent).join('\r\n')}
+	END:VCALENDAR`;
 
 	let file = new Blob([icsContent], { type: 'text/calendar' });
 	const a = document.createElement("a");
